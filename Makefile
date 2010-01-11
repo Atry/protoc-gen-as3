5a4a8b67ba8d79329b99a7249f7ca7bb0ed55bf3
yangbo <yangbo@localhost>
2010-01-11 11:32:38 +0800

    ???????????

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
+# Copyright (c) 2010 , 杨博 (Yang Bo) All rights reserved.
+#
+#         pop.atry@gmail.com
+#
+# Use, modification and distribution are subject to the "New BSD License"
+# as listed at <url: http://www.opensource.org/licenses/bsd-license.php >.
 include config.mk
 
 all: com/netease/protocGenAs3/Main.class
--- com/netease/protocGenAs3/Main.java
+++ com/netease/protocGenAs3/Main.java
@@ -1,4 +1,12 @@
 // vim: tabstop=4 shiftwidth=4
+
+// Copyright (c) 2010 , 杨博 (Yang Bo) All rights reserved.
+//
+//         pop.atry@gmail.com
+//
+// Use, modification and distribution are subject to the "New BSD License"
+// as listed at <url: http://www.opensource.org/licenses/bsd-license.php >.
+
 package com.netease.protocGenAs3;
 import static google.protobuf.compiler.Plugin.*;
 import static com.google.protobuf.DescriptorProtos.*;
